610a04e06880337398ac837654d703efa6d97cd4
profedugamer <114632216+profedugamer@users.noreply.github.com>
2025-01-27 02:17:28 +0000

    operadores relacionais e super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,13 +9,27 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    //Variáveis da primeira carta
     char codigo_da_cidade[4];
     char nome[50];
     int populacao;
     float area;
     double pib;
     int pontos_turisticos;
-    float densidade_populacional, pib_per_capita;
+    unsigned int densidade_populacional;
+    float pib_per_capita, superpoder;
+
+
+    //variáveis da segunda carta
+    char codigo_da_cidade2[4];
+    char nome2[50];
+    int populacao2;
+    float area2;
+    double pib2;
+    int pontos_turisticos2;
+    unsigned int densidade_populacional2;
+    float pib_per_capita2, superpoder2;
 
 
     // Descrição do jogo Super Trunfo Países
@@ -30,7 +44,8 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf("Insira o código da carta: \n");
+    //Entrada de dados da primeira carta
+    printf("Insira o código da primeira carta: \n");
     scanf("%s", &codigo_da_cidade);
     printf("Insira o nome da cidade: \n");
     scanf(" %[^\n]", &nome); //Entrada para aceitar espaço no nome das cidades. Comando: %[^\n]
@@ -43,19 +58,45 @@ int main() {
     printf("Insira a quantidade de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos);
 
+    //Entrada de dados da segunda carta
+    printf("\nInsira o código da segunda carta: \n");
+    scanf("%s", &codigo_da_cidade2);
+    printf("Insira o nome da cidade: \n");
+    scanf(" %[^\n]", &nome2); //Entrada para aceitar espaço no nome das cidades. Comando: %[^\n]
+    printf("Insira a população da cidade: \n");
+    scanf("%d", &populacao2);
+    printf("Insira o valor da área (km²): \n");
+    scanf("%f", &area2);
+    printf("Insira o PIB: \n");
+    scanf("%lf", &pib2);
+    printf("Insira a quantidade de pontos turísticos: \n");
+    scanf("%d", &pontos_turisticos2);
+
     //Desafio aventureiro:
     //Foram adicionadas duas variáveis novas no inicio do documento. Elas são: Densidade populacional e PIB per Capita
     //Essas variáveis serão calculadas automaticamente
     //Densidade Populacional: População dividida pela área da cidade.
     //PIB per Capita: PIB total dividido pela população.
-    densidade_populacional = (float) populacao / area;
+
+    //Cálculos do jogo Supertrunfo:
+    //Densidade da primeira carta
+    densidade_populacional = (unsigned int) populacao / area;
     pib_per_capita = (float) pib / populacao;
+    
+    //Densidade da segunda carta
+    densidade_populacional2 = (unsigned int) populacao2 / area2;
+    pib_per_capita2 = (float) pib2 / populacao2;
+
+    //Desafio 3 - Mestre - Cálculo do Superpoder - Primeira carta e segunda carta
+    superpoder = (float) populacao + area + pib + pontos_turisticos + densidade_populacional + pib_per_capita;
+    superpoder2 = (float) populacao2 + area2 + pib2 + pontos_turisticos2 + densidade_populacional2 + pib_per_capita2;
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("\nCódigo da carta: %s\n", codigo_da_cidade);
+    //Exibição das características da primeira carta
+    printf("\nCódigo da primeira carta: %s\n", codigo_da_cidade);
     printf("Cidade: %s\n", nome);
     printf("População: %d\n", populacao);
     printf("Área %.2f km²\n", area);
@@ -63,8 +104,38 @@ int main() {
     printf("Pontos Turisticos: %d\n", pontos_turisticos);
 
     //Novos printf do programa - nível aventureiro
-    printf("A densidade populacional é: %.2f\n", densidade_populacional);
+    printf("A densidade populacional é: %u\n", densidade_populacional);
     printf("O PIB per Capita é: %.2f\n", pib_per_capita);
+
+    //Adicionado superpoder da primeira carta
+    printf("Super poder: %.2f\n", superpoder);
+
+    //Exibição das características da segunda carta
+    printf("\nCódigo da segunda carta: %s\n", codigo_da_cidade2);
+    printf("Cidade: %s\n", nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área %.2f km²\n", area2);
+    printf("PIB: %.2lf\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontos_turisticos2);
+
+    //Novos printf do programa - nível aventureiro
+    printf("A densidade populacional é: %u\n", densidade_populacional2);
+    printf("O PIB per Capita é: %.2f\n", pib_per_capita2);
+
+    //Adicionado superpoder da segunda carta
+    printf("Super poder: %.2f\n", superpoder2);
+
+    //Desafio Mestre - Comparação das cartas
+    //Densidade populacional: vence a carta com menor valor
+    //Outras propriedades: vence carta com maior valor
+
+    printf("\npopulacao > populacao2: %d\n", populacao > populacao2);
+    printf("area > area2: %d\n", (int) area > area2);
+    printf("pib > pib2: %d\n", (int) pib > pib2);
+    printf("pontos_turisticos > pontos_turisticos2: %d\n", pontos_turisticos > pontos_turisticos2);
+    printf("densidade_populacional < densidade_populacional2: %u\n", densidade_populacional < densidade_populacional2);
+    printf("pib_per_capita > pib_per_capita2: %d\n", (int) pib_per_capita > pib_per_capita2);
+    printf("superpoder > superpoder2: %d\n", (int) superpoder > superpoder2);
 
     return 0;
 }
